9edbbc113c8ce60878316ab5d32f949b6c6af621
Martin Krauskopf <martin.krauskopf@gmail.com>
2009-08-26 17:36:06 +0000

    One more try for the right value for default 'host' value.
    127.0.0.1 seemingly works with all systems and with IPv6 as well.
    "localhost" and nil on have problems on some systems.

--- lib/ruby-debug.rb
+++ lib/ruby-debug.rb
@@ -119,11 +119,10 @@ module Debugger
       return if @control_thread
       @control_thread = DebugThread.new do
         begin
-          unless RUBY_PLATFORM =~ /darwin/i or # Mac OS X seems to have problem with 'localhost'
-                 RUBY_VERSION  =~ /1\.9\../    # Ruby 1.9.x seems to as well
-            host ||= 'localhost' # nil does not seem to work for IPv6, localhost does
-          end
           $stderr.printf "Fast Debugger (ruby-debug-ide 0.4.7) listens on #{host}:#{port}\n"
+          # 127.0.0.1 seemingly works with all systems and with IPv6 as well.
+          # "localhost" and nil on have problems on some systems.
+          host ||= '127.0.0.1'
           server = TCPServer.new(host, port)
           while (session = server.accept)
             begin
