4197e901591649ca32be2d1ccdb8d301bbbb6264
Dennis Ushakov <dennis.ushakov@gmail.com>
2010-12-13 18:56:15 +0000

    fix tests running

--- test/rd_test_base.rb
+++ test/rd_test_base.rb
@@ -22,7 +22,7 @@ class RDTestBase < TestBase
     cmd = "#{interpreter}"
     cmd << " --debug" if jruby?
     cmd << " -J-Xdebug -J-Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y" if jruby? and debug_jruby?
-    cmd << " -I '#{File.dirname(script)}' #{@rdebug_ide}" +
+    cmd << " -I 'lib:#{File.dirname(script)}' #{@rdebug_ide}" +
       (@verbose_server ? " -d" : "") +
       " -p #{port} -- '#{script}'"
   end
