12fc83ce1f389c1feaaa3899dd716b670909687a
Dennis Ushakov <dennis.ushakov@gmail.com>
2011-02-02 11:25:25 +0000

    fix installation issues with prerelease base gem versions

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -2,6 +2,7 @@ jruby = defined?(JRUBY_VERSION) || (defined?(RUBY_ENGINE) && 'jruby' == RUBY_ENG
 unless jruby
   require 'rubygems'
   require 'rubygems/command.rb'
+  require 'rubygems/dependency.rb'
   require 'rubygems/dependency_installer.rb'
 
   begin
@@ -9,15 +10,24 @@ unless jruby
     rescue NoMethodError
   end
 
-  inst = Gem::DependencyInstaller.new(:prerelease => true)
+  if RUBY_VERSION < "1.9"
+    dep = Gem::Dependency.new("ruby-debug-base", '>=0.10.4')
+  else
+    dep = Gem::Dependency.new("ruby-debug-base19x", '>=0.11.24')
+  end
+
+  inst = Gem::DependencyInstaller.new
   begin
-    if RUBY_VERSION < "1.9"
-      inst.install "ruby-debug-base", '>=0.10.4'
-    else
-      inst.install "ruby-debug-base19x", '>=0.11.24'
-    end
-    rescue
+    inst.install dep
+  rescue
+    inst = Gem::DependencyInstaller.new(:prerelease => true)
+    begin
+      inst.install dep
+    rescue Exception => e
+      puts e
+      puts e.backtrace.join "\n  "
       exit(1)
+    end
   end
 end
 
