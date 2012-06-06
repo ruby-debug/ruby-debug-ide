b14001fdaec6417e425e501eb6876e2fefe8aba4
Dennis Ushakov <dennis.ushakov@gmail.com>
2012-06-06 15:42:34 +0400

    ensure ruby-debug-ide is loaded on windows

--- lib/ruby-debug-ide/multiprocess/starter.rb
+++ lib/ruby-debug-ide/multiprocess/starter.rb
@@ -1,5 +1,6 @@
 if ENV['IDE_PROCESS_DISPATCHER']
   require 'rubygems'
+  $: << File.expand_path(File.dirname(__FILE__)) + "../.."
   require 'ruby-debug-ide'
   require 'ruby-debug-ide/multiprocess'
   Debugger::MultiProcess::pre_child
--- lib/ruby-debug-ide/version.rb
+++ lib/ruby-debug-ide/version.rb
@@ -1,3 +1,3 @@
 module Debugger
-  IDE_VERSION='0.4.17.beta10'
+  IDE_VERSION='0.4.17.beta11'
 end
