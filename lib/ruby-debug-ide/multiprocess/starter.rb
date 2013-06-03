477c3c590aec9a1d25b38089cb98030db5264ad1
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2013-06-03 20:27:51 +0400

    do not filter out debase path in subprocesses

--- lib/ruby-debug-ide/multiprocess/starter.rb
+++ lib/ruby-debug-ide/multiprocess/starter.rb
@@ -1,7 +1,7 @@
 if ENV['IDE_PROCESS_DISPATCHER']
   require 'rubygems'
   ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
-    next unless path =~ /ruby-debug-ide|ruby-debug-base|linecache/
+    next unless path =~ /ruby-debug-ide|ruby-debug-base|linecache|debase/
     $LOAD_PATH << path
   end
   require 'ruby-debug-ide'
