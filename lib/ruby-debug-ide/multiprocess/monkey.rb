36060b08b2b71261ff53ff15a4b37aa95a23a8d4
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2013-09-09 16:43:32 +0400

    fixed exec/fork monkeypathing on Kernel

--- lib/ruby-debug-ide/multiprocess/monkey.rb
+++ lib/ruby-debug-ide/multiprocess/monkey.rb
@@ -1,9 +1,10 @@
 module Debugger
   module MultiProcess
-    def self.create_mp_fork
+    def self.create_mp_fork(private=false)
       %Q{
         alias pre_debugger_fork fork
     
+        #{private ? "private" : ""}
         def fork(*args)
           if block_given?
             return pre_debugger_fork{Debugger::MultiProcess::pre_child; yield}
@@ -15,10 +16,11 @@ module Debugger
       }
     end
 
-    def self.create_mp_exec
+    def self.create_mp_exec(private=false)
       %Q{
         alias pre_debugger_exec exec
-    
+        
+        #{private ? "private" : ""}
         def exec(*args)
           Debugger.interface.close
           pre_debugger_exec(*args)
@@ -33,8 +35,8 @@ module Kernel
     module_eval Debugger::MultiProcess.create_mp_fork
     module_eval Debugger::MultiProcess.create_mp_exec
   end
-  module_eval Debugger::MultiProcess.create_mp_fork
-  module_eval Debugger::MultiProcess.create_mp_exec
+  module_eval Debugger::MultiProcess.create_mp_fork(true)
+  module_eval Debugger::MultiProcess.create_mp_exec(true)
 end
 
 module Process
