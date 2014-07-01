ac48c48ebdbf4183f74237071c2621f82abfb0b9
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2014-07-01 16:11:23 +0400

    rename and move 'more' command to expression_info; set string encoding for stringIO

---
+++ lib/ruby-debug-ide/commands/expression_info.rb
@@ -0,0 +1,56 @@
+require 'stringio'
+require 'irb/ruby-lex'
+
+module Debugger
+
+  class ExpressionInfoCommand < Command
+    def regexp
+      /^\s*ex(?:pression_info)?\s+/
+    end
+
+    def execute
+      string_to_parse = @match.post_match.gsub("\\n", "\n") + "\n\n\n"
+
+      lexer = RubyLex.new
+      io = StringIO.new(string_to_parse)
+      # for passing to the lexer
+      io.instance_exec(string_to_parse.encoding) do |string_encoding|
+        @my_encoding = string_encoding
+        def self.encoding
+          @my_encoding
+        end
+      end
+
+      lexer.set_input(io)
+      lexer.set_prompt {|ltype, indent, continue, lineno| }
+
+      last_statement = '',0
+      lexer.each_top_level_statement do |line, line_no|
+        last_statement = line, line_no
+      end
+
+      result = true
+      if /\A\s*\Z/m =~ last_statement[0]
+        result = false
+      end
+
+      @printer.print_pp("<expressionInfo incomplete=\"#{result.to_s}\"></expressionInfo>")
+    end
+
+    class << self
+      def help_command
+        "expression_info"
+      end
+
+      def help(cmd)
+        %{
+          ex[pression_info] <expression>\t
+          returns parser-related information for the expression given\t\t
+          'incomplete'=true|false\tindicates whether expression is a complete ruby
+          expression and can be evaluated without getting syntax errors
+        }
+      end
+    end
+  end
+
+end
--- lib/ruby-debug-ide/commands/inspect.rb
+++ lib/ruby-debug-ide/commands/inspect.rb
@@ -22,38 +22,4 @@ module Debugger
     end
   end
 
-  class MoreCommand < Command
-    def regexp
-      /^\s*more\s+/
-    end
-
-    def execute
-      require 'stringio'
-      require 'irb/ruby-lex'
-
-      string_to_parse = @match.post_match.gsub("\\n", "\n") + "\n\n\n"
-
-      lexer = RubyLex.new
-      io = StringIO.new(string_to_parse)
-      # for passing to the lexer
-      def io.encoding
-        "utf-8"
-      end
-      lexer.set_input(io)
-      lexer.set_prompt {|ltype, indent, continue, lineno| }
-
-      last_statement = '',0
-      lexer.each_top_level_statement do |line, line_no|
-        last_statement = line, line_no
-      end
-
-      result = 1
-      if /\A\s*\Z/m =~ last_statement[0]
-        result = 0
-      end
-
-      @printer.print_pp("<more value=\"#{result.to_s}\"></more>")
-    end
-  end
-
 end
