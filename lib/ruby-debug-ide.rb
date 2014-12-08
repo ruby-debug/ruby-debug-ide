20a3a3cb995b859a18fee0ed25438f7c3ca73d00
Oleg Sukhodolsky <os97673@gmail.com>
2014-12-08 13:53:30 +0300

    Need to declare Debugger.rm_protocol_extensions

--- lib/ruby-debug-ide.rb
+++ lib/ruby-debug-ide.rb
@@ -41,7 +41,7 @@ module Debugger
        cleared
     end
 
-    attr_accessor :cli_debug, :xml_debug, :evaluation_timeout
+    attr_accessor :cli_debug, :xml_debug, :evaluation_timeout, :rm_protocol_extensions
     attr_accessor :control_thread
     attr_reader :interface
 
