5a960314cb35e379dcb3af2b46a4ba1586d3e453
Will Roden <willabides@github.com>
2016-03-13 17:28:01 -0500

    check for nil DEBUGGER_STORED_RUBYLIB before trying to split it

--- lib/ruby-debug-ide/multiprocess/starter.rb
+++ lib/ruby-debug-ide/multiprocess/starter.rb
@@ -1,8 +1,10 @@
 if ENV['IDE_PROCESS_DISPATCHER']
   require 'rubygems'
-  ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
-    next unless path =~ /ruby-debug-ide|ruby-debug-base|linecache|debase/
-    $LOAD_PATH << path
+  unless ENV['DEBUGGER_STORED_RUBYLIB'].nil?
+    ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
+      next unless path =~ /ruby-debug-ide|ruby-debug-base|linecache|debase/
+      $LOAD_PATH << path
+    end
   end
   require 'ruby-debug-ide'
   require 'ruby-debug-ide/multiprocess'
