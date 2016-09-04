36d3686d31106024082d6855bfdfd3c2cdfbe663
equivalence1 <equivalence1@gmail.com>
2016-09-04 22:07:20 +0300

    some cosmetic changes + choosing with debugger to use

--- ext/do_attach.c
+++ ext/do_attach.c
@@ -1,7 +1,5 @@
 #include "do_attach.h"
 
-static const char *_command_to_eval;
-
 static int
 __check_gc(void)
 {
@@ -29,17 +27,13 @@ __catch_line_event(rb_event_flag_t evflag, VALUE data, VALUE self, ID mid, VALUE
     if (__check_gc())
         return;
     __func_to_set_breakpoint_at();
-//    rb_eval_string_protect(_command_to_eval, NULL); // TODO pass something more useful than NULL
 }
 
 int
-start_attach(const char* command)
+start_attach()
 {
-    rb_eval_string("puts 'bla bla bla'");
-    _command_to_eval = command;
     if (__check_gc())
         return 1;
-    rb_global_variable((VALUE *) _command_to_eval);
     rb_add_event_hook(__catch_line_event, RUBY_EVENT_LINE, (VALUE) NULL);
-    return 2323;
+    return 0;
 }
--- ext/do_attach.h
+++ ext/do_attach.h
@@ -5,6 +5,6 @@
 #include <ruby/debug.h>
 #include <stdio.h>
 
-int start_attach(const char *command);
+int start_attach();
 
 #endif //__DO_ATTACH_H__
