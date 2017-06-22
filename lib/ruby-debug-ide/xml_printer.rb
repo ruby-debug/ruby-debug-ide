e9d9661cd10e9ef7fbf7548611c5b1436fe9c0f0
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2017-06-22 17:29:31 +0300

    inspect memory limit fix

--- lib/ruby-debug-ide/xml_printer.rb
+++ lib/ruby-debug-ide/xml_printer.rb
@@ -1,6 +1,7 @@
 require 'stringio'
 require 'cgi'
 require 'monitor'
+require 'objspace'
 
 module Debugger
 
@@ -95,6 +96,7 @@ module Debugger
     end
     
     def print_variables(vars, kind)
+      
       print_element("variables") do
         # print self at top position
         print_variable('self', yield('self'), kind) if vars.include?('self')
@@ -139,6 +141,7 @@ module Debugger
     end
     
     def print_variable(name, value, kind)
+      
       name = name.to_s
       if value.nil?
         print("<variable name=\"%s\" kind=\"%s\"/>", CGI.escapeHTML(name), kind)
@@ -170,7 +173,10 @@ module Debugger
         rescue
         end
       end
+
+
       value_str = handle_binary_data(value_str)
+      
       escaped_value_str = CGI.escapeHTML(value_str)
       print("<variable name=\"%s\" %s kind=\"%s\" %s type=\"%s\" hasChildren=\"%s\" objectId=\"%#+x\">",
           CGI.escapeHTML(name), build_compact_value_attr(value, value_str), kind,
@@ -380,14 +386,45 @@ module Debugger
       50
     end
 
+    def inspect_with_allocation_control(slice)
+      x = Thread.current
+      
+      start_alloc_size = ObjectSpace.memsize_of_all
+      
+      trace = TracePoint.new(:c_call, :call) do |tp|
+        curr_alloc_size = ObjectSpace.memsize_of_all
+        
+        if(curr_alloc_size - start_alloc_size > 1e7)
+          
+          trace.disable
+          x.raise StandardError, "Out of memory: evaluation took longer than 10mb." if x.alive?
+        end
+        p [tp.path, tp.lineno, tp.event]
+      end
+
+      trace.enable
+      slice.inspect
+      trace.disable 
+    end
+    
+
     def compact_array_str(value)
       slice   = value[0..10]
-      compact = slice.inspect
+      
+      if defined?(JRUBY_VERSION)
+        compact = slice.inspect
+      else  
+        compact = inspect_with_allocation_control(slice)
+      end 
+      
       if value.size != slice.size
         compact[0..compact.size-2] + ", ...]"
       end
       compact
+    rescue StandardError => e
+      return nil
     end
+
 
     def compact_hash_str(value)
       slice   = value.sort_by { |k, _| k.to_s }[0..5]
