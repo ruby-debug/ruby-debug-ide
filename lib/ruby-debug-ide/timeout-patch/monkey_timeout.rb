778da60939c5a78e3551549e79959879c6b24b1f
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2017-08-04 20:42:31 +0300

    refactoring: %q to block for module_eval
    (cherry picked from commit 4b62b62)

--- lib/ruby-debug-ide/timeout-patch/monkey_timeout.rb
+++ lib/ruby-debug-ide/timeout-patch/monkey_timeout.rb
@@ -1,57 +1,49 @@
-module Debugger
-  module TimeoutHandler
-    def self.create_mp_timeout
-      %q{
-        alias pre_eval_timeout timeout
-
-        def timeout(sec, klass = nil)   #:yield: +sec+
-          return yield(sec) if sec == nil or sec.zero?
-          message = "execution expired".freeze
-          from = "from #{caller_locations(1, 1)[0]}" if $DEBUG
-          e = Error
-          bl = proc do |exception|
-            begin
-              x = Thread.current
-              y = Debugger::DebugThread.start {
-                Thread.current.name = from
-                begin
-                  sleep sec
-                rescue => e
-                  x.raise e
-                else
-                  x.raise exception, message
-                end
-              }
-              return yield(sec)
-            ensure
-              if y
-                y.kill
-                y.join # make sure y is dead.
-              end
-            end
-          end
-          if klass
-            begin
-              bl.call(klass)
-            rescue klass => e
-              bt = e.backtrace
-            end
-          else
-            bt = Error.catch(message, &bl)
-          end
-          level = -caller(CALLER_OFFSET).size-2
-          while THIS_FILE =~ bt[level]
-            bt.delete_at(level)
-          end
-          raise(e, message, bt)
-        end
-      }
-    end
-  end
-end
-
 module Timeout
   class << self
-    module_eval Debugger::TimeoutHandler.create_mp_timeout
+    module_eval {
+      alias pre_eval_timeout timeout
+
+      def timeout(sec, klass = nil) #:yield: +sec+
+        return yield(sec) if sec == nil or sec.zero?
+        message = "execution expired".freeze
+        from = "from #{caller_locations(1, 1)[0]}" if $DEBUG
+        e = Error
+        bl = proc do |exception|
+          begin
+            x = Thread.current
+            y = Debugger::DebugThread.start {
+              Thread.current.name = from
+              begin
+                sleep sec
+              rescue => e
+                x.raise e
+              else
+                x.raise exception, message
+              end
+            }
+            return yield(sec)
+          ensure
+            if y
+              y.kill
+              y.join # make sure y is dead.
+            end
+          end
+        end
+        if klass
+          begin
+            bl.call(klass)
+          rescue klass => e
+            bt = e.backtrace
+          end
+        else
+          bt = Error.catch(message, &bl)
+        end
+        level = -caller(CALLER_OFFSET).size - 2
+        while THIS_FILE =~ bt[level]
+          bt.delete_at(level)
+        end
+        raise(e, message, bt)
+      end
+    }
   end
 end
