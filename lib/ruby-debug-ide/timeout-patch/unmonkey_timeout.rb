d0a846fe33e5e85c87fdfa3fd0a3a9333eaf8c43
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2017-08-04 20:42:31 +0300

    refactoring: %q to block for module_eval

--- lib/ruby-debug-ide/timeout-patch/unmonkey_timeout.rb
+++ lib/ruby-debug-ide/timeout-patch/unmonkey_timeout.rb
@@ -1,13 +1,7 @@
-module Debugger
-  module TimeoutHandler
-    class << self
-      def do_timeout_monkey
-        load File.expand_path(File.dirname(__FILE__) + '/timeout-patch/monkey_timeout.rb')
-      end
-
-      def undo_timeout_monkey
-        load File.expand_path(File.dirname(__FILE__) + '/timeout-patch/unmonkey_timeout.rb')
-      end
-    end
+module Timeout
+  class << self
+    module_eval {
+      alias timeout pre_eval_timeout
+    }
   end
 end
